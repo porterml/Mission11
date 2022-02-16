426ec7b4e19cfa1a6f49654f5810afc078949be3
porterml <90496293+porterml@users.noreply.github.com>
2022-02-16 16:05:26 -0700

    Add project files.

---
+++ AmazonBookStore/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using AmazonBookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonBookStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        private BookstoreContext context { get; set; }
+
+        public HomeController (BookstoreContext temp)
+        {
+            context = temp;
+        }
+
+        public IActionResult Index()
+        {
+            var blah = context.Books.ToList();
+
+            return View(blah);
+        }
+
+    }
+}
